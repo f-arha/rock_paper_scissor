17dfe85f3149888d753eb758bc3f014981f5a344
f-arha <190675045+f-arha@users.noreply.github.com>
2025-02-15 14:04:55 +0530

    Add function to calculate score

--- script.js
+++ script.js
@@ -21,3 +21,42 @@ function getHumanChoice(){
     console.log("You:"+h);
     return h;
 }
+
+function playRound(humanChoice,computerChoice){
+    if(humanChoice=="paper" && computerChoice=="rock" || humanChoice=="rock" && computerChoice=="scissor" || humanChoice=="scissor" && computerChoice=="paper"){
+     console.log("You win! "+humanChoice+" beats "+computerChoice);
+     humanScore++;
+    }
+    else if(humanChoice=="rock" && computerChoice=="paper" || humanChoice=="paper" && computerChoice=="scissor" || humanChoice=="scissor" && computerChoice=="rock"){
+     console.log("You lose!"+computerChoice+" beats "+humanChoice);
+     computerScore++;
+    }else if(humanChoice==computerChoice){
+         console.log("It's a tie! Both chose " + humanChoice);
+        
+    }else{
+     console.log("wrong input");
+    }
+    
+    
+ }
+ 
+ 
+ function playGame(){
+     for(let i=0;i<5;i++){
+         const humanSelection=getHumanChoice();
+         const computerSelection=getComputerChoice();
+         playRound(humanSelection,computerSelection);
+         console.log("human:"+humanScore);
+         console.log("computer:"+computerScore);
+     }
+     if(humanScore<computerScore){
+         console.log("COMPUTER WINS")
+     }
+     else if(humanScore>computerScore){
+         console.log("You WINS");
+     }
+     else{
+         console.log("It's a tie");
+     }
+ }
+ playGame();
